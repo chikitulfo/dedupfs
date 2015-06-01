538672e5e3882be82bec2d26b685c989a07688dc
Chikitulfo <chikitulfix@gmail.com>
2015-06-01 21:09:34 +0200

    Finalizado

--- vagrantbootstrap.sh
+++ vagrantbootstrap.sh
@@ -2,5 +2,5 @@
 
 apt-get update
 apt-get install -y libssl-dev pkg-config libfuse2 libfuse-dev \
-	sqlite3 sqlite3-doc libsqlite3-devcd 
+	sqlite3 sqlite3-doc libsqlite3-dev
 ln -sf /vagrant /home/vagrant/dedupfs
